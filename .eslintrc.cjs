ce3a64ad0f5d0db771d93eee3716ee1e7738c40f
Gyubeom Park <qja0707@gmail.com>
2026-01-02 18:01:56 +0900

    chore: add lint setting

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  rules: {
+    indent: ['error', 2],
+    semi: ['error', 'always']
+  }
+};
